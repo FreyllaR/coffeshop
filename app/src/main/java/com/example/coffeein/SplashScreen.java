ced6755ccd94e46a1133c38d289fd9f4a91f453a
Kirill <olkov.kir1@yandex.ru>
2023-03-25 20:26:57 +0300

    adding a splash screen

---
+++ app/src/main/java/com/example/coffeein/SplashScreen.java
@@ -0,0 +1,34 @@
+package com.example.coffeein;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        Thread welcomeThread = new Thread() {
+
+            @Override
+            public void run() {
+                try {
+                    super.run();
+                    sleep(2400);  //Delay of 10 seconds
+                } catch (Exception e) {
+
+                } finally {
+
+                    Intent i = new Intent(SplashScreen.this,
+                            MainActivity.class);
+                    startActivity(i);
+                    finish();
+                }
+            }
+        };
+        welcomeThread.start();
+    }
+}
